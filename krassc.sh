6d2ce40945a8be7947a56033a36eb5f3cdf9cdc3
Keith Allatt <keith.allatt@gmail.com>
2019-09-15 16:26:49 -0400

    Update krassc.sh

--- krassc.sh
+++ krassc.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# Krass Compiler Shell Script:
+#     Takes care of passing file contents to a compiler written in a 
+#     different language. After it is all parsed through into pure TeX, 
+#     the resultant TeX file will be passed through a TeX compiler and
+#     the final PDF file will be kept.
+
 echo "Krass Compiler Shell Script";
