0988d721bd1634587c3b46daa6781b06255af01f
Keith Allatt <keith.allatt@gmail.com>
2019-09-16 10:09:06 -0400

    Create krass2python.py

---
+++ krass2python.py
@@ -0,0 +1,9 @@
+"""
+Create a *.py file based on krass components. 
+After the file is created, it will be run by 
+the krassc.sh script to gain the output. That
+output will be inserted into the final TeX document.
+"""
+
+if __name__ == "__main__":
+  pass
