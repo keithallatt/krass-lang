139ce4d5b1f08460284321a53a413b98f53fc4d9
Keith Allatt <keith.allatt@gmail.com>
2019-09-25 14:30:21 -0400

    Added alias making to setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,20 @@
 mkdir ~/.krass-lang
 
 # copy all files over.
-cp ./* ~/.krass-lang/
+cp -r ./* ~/.krass-lang/
+
+bp=$(<~/.bash_profile)
+
+if [[ $bp != *"alias krassc"* ]]; then 
+	
+	echo "alias krassc='~/.krass-lang/krassc.sh'" >> ~/.bash_profile
+		
+	source ~/.bash_profile
+
+	echo "Alias krass now exists";
+else
+	echo "Alias krassc already exists";
+fi
+
 
 echo "Setup complete."
